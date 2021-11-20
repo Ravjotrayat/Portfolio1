fa4ffc896f8b856856a47ebbdad66c590922f511
Ravjotrayat <94758593+Ravjotrayat@users.noreply.github.com>
2021-11-20 23:49:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portfolio</title>
+    <style>
+        .Avatar
+        {
+         border-radius: 50%;
+         height: 150px;
+         width: 150px;
+        
+        }
+        #hi{
+            border-radius: 8px;
+            background-color: rgb(253, 253, 60);
+        }
+    </style>
+</head>
+<body >
+    
+<br>
+<center><div style="font-size: 60px; font-style: italic; font-weight: bolder;">My Portfolio</div><br><br>
+<fieldset style=" background-color: rgb(47, 34, 230);height: 240px;">
+    
+   <center> <legend><img class="Avatar"  src="my image1.jpg" alt="Avatar" > <br><em><strong style="text-decoration-line: underline;">RAVJOT SINGH RAYAT</strong></em><br><br>
+    <input type="button" value="Front End Developer" id="hi"><br>
+<table >
+    <tr><table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" >
+        <tr>
+            <td> <font face="cursive" size="4.5"> About me</font></td>
+            <td>|</td>
+            <td><font face="cursive" size="4.5">Resume</font> </td>
+            <td>|</td>
+            <td> <font face="cursive" size="4.5">Contact me</font></td>
+            <td>|</td>
+            <td> <font face="cursive" size="4.5">Resources</font> </td>
+        </tr>
+    </table></tr>
+    
+</table>
+<div>
+
+</div>
+<font style="font-size: 30px;"><strong>About me :</strong> </font> <font style="font-size: 25px;">
+    <center>Hi! I am Ravjot Singh Rayat and currently i am studying in <em> "Gandhi Institute of Engineering and Technology University</em>."I am a Native of Jamshedpur,Jharkhand. I am learning Front End Developer.</center>
+</font><br>
+<font style="font-size: 30px;"><strong> Contact me :
+ </strong></font><br> <font style="font-size: 30px; color: ;"><a href="https://mail.google.com/mail/u/0/#inbox?compose=new">rayat.ravjotsingh@gmail.com</a>  <br>
+ </font>
+</legend>
+</fieldset>
+</center>
+</fieldset></center>
+</body>
+</html>
